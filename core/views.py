6e617bf8cb10ba93ce16ccb3529804ac097b625e
Tin [Yue Tian] <iamtin@gmail.com>
2010-12-07 11:01:09 +0800

    实现了jsonp方式的嵌入，为跨域访问做好准备

--- core/views.py
+++ core/views.py
@@ -7,6 +7,9 @@ from models import TimelineVisitor
 def timeline(request, user_id):
     visitor = TimelineVisitor(user_id)
     timeline = visitor.get_timeline()
-
-    return HttpResponse(json.dumps(timeline),
+    callback = request.GET.get('callback', None)
+    response_text = json.dumps(timeline)
+    if callback:
+        response_text = '%s(%s);' % (callback, response_text)
+    return HttpResponse(response_text,
                         mimetype='application/json')
